f60d78823d7bdd9c091061c03d53b0f2a4ec5ed1
Adari Bain <lilchris438@gmail.com>
2026-06-17 22:13:10 -0400

    Update kube-bench.sh

--- scripts/cluster-hardening-scripts/011-security-tools/kube-bench/kube-bench.sh
+++ scripts/cluster-hardening-scripts/011-security-tools/kube-bench/kube-bench.sh
@@ -40,7 +40,7 @@ log_step "Installing kube-bench..."
 
 curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.8.0/kube-bench_0.8.0_linux_amd64.tar.gz -o kube-bench_0.8.0_linux_amd64.tar.gz
 tar -xvf kube-bench_0.8.0_linux_amd64.tar.gz
-
+sudo mv kube-bench /usr/local/bin/
 
 # Verify installation
 if command -v kube-bench &> /dev/null; then
